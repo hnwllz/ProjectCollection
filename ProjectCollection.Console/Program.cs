95d0c893349604c3a94f2ed3e0e9ce6a2a37603d
longzhi <hnwllz@163.com>
2018-10-19 01:18:19 +0800

    提交生成测试数据

--- ProjectCollection.Console/Program.cs
+++ ProjectCollection.Console/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,22 +13,37 @@ namespace ProjectCollection.Console
     {
         static void Main(string[] args)
         {
-            //NumberDataGenerator gen = new NumberDataGenerator();
-            //gen.MinValue = -400000;
-            //gen.MaxValue = 400000;
-            //gen.FilePath = @"E:\数据\测试数据\numbers.txt";
-            //gen.Generator(400000);
 
-            //System.Console.WriteLine("生成完成");
-
+            GenerateNums();
+            /*
             int a = int.MinValue;
-            int n = 31;
+            int n = 32;
             while (n-- > 0)
             {
-                a = a >> 1;
+                a = a / 2;
             }
             Console1.WriteLine(a);
+            */
             Console1.ReadKey();
+        }
+
+        static void GenerateNums()
+        {
+            string director = "../../../ProjectCollection.Test/Data";
+            int countOfnums = 10;
+            while (countOfnums <= Math.Pow(10, 7))
+            {
+                System.Console.WriteLine("生成数量{0}的文件",countOfnums);
+                NumberDataGenerator gen = new NumberDataGenerator();
+                gen.MinValue = -countOfnums;
+                gen.MaxValue = countOfnums;
+                gen.FilePath = Path.Combine(director, string.Format("nums{0}.txt", countOfnums));
+                gen.Generator(countOfnums);
+
+                countOfnums *= 10;
+            }
+
+            System.Console.WriteLine("生成完成");
         }
     }
 }
